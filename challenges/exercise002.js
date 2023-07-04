3920b00e1c320c778b534fd084f6f78299099271
Your GitHub Usernamemsdubova <femme777@gmail.com>
2023-07-04 19:57:58 +0300

    Exercise 002

--- challenges/exercise002.js
+++ challenges/exercise002.js
@@ -1,24 +1,24 @@
 export function getFillings(sandwich) {
 	if (sandwich === undefined) throw new Error('ingredients is required');
-	// Your code here!
+	return sandwich.fillings;
 }
 
 export function isFromManchester(person) {
 	if (person === undefined) throw new Error('person is required');
-	// Your code here!
+	return person.city === 'Manchester' ? true: false;
 }
 
 export function getBusNumbers(people) {
 	if (people === undefined) throw new Error('people is required');
-	// Your code here!
+	return people <= 40 ? 1 : Math.ceil(people/40)
 }
 
 export function countSheep(arr) {
 	if (arr === undefined) throw new Error('arr is required');
-	// Your code here!
+	return arr.reduce((count, animal)=> {return animal === 'sheep'? count +1 : count}, 0)
 }
 
 export function hasMPostCode(person) {
 	if (person === undefined) throw new Error('person is required');
-	// Your code here!
+	return person.address.postCode.charAt(0)==='M' ? person.address.city ==='Manchester' ? true : false: false;
 }
