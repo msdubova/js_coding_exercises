601b167ff2dd3929c57a8ce3421386611d658d4e
Your GitHub Usernamemsdubova <femme777@gmail.com>
2023-07-04 22:01:50 +0300

    Exercise 003

--- challenges/exercise003.js
+++ challenges/exercise003.js
@@ -1,26 +1,32 @@
 export function getSquares(nums) {
 	if (nums === undefined) throw new Error('nums is required');
-	// Your code here!
+	return nums.map((a)=> Math.pow(a,2))
 }
 
 export function camelCaseWords(words) {
 	if (words === undefined) throw new Error('words is required');
-	// Your code here!
+	
+	return words.map((word, index) => {
+	return index == 0 ? word : word.charAt(0).toUpperCase() + word.slice(1).toLowerCase();
+	}).join('');
 }
 
 export function getTotalSubjects(people) {
 	if (people === undefined) throw new Error('people is required');
-	// Your code here!
+	return people.reduce((count, a)=>{ return (count + a.subjects.length) },0)
 }
 
 export function checkIngredients(menu, ingredient) {
 	if (menu === undefined) throw new Error('menu is required');
 	if (!ingredient) throw new Error('ingredient is required');
-	// Your code here!
+	return menu.some((dish) => dish.ingredients.includes(ingredient));
 }
 
 export function duplicateNumbers(arr1, arr2) {
 	if (arr1 === undefined) throw new Error('arr1 is required');
 	if (arr2 === undefined) throw new Error('arr2 is required');
-	// Your code here!
+	const arr = [];
+	arr1.forEach(item => (arr2.includes(item) && !arr.includes(item)) ? arr.push(item) : null);
+	return arr.sort((a, b)=>a - b);
 }
+ 
