4913296044f538899fbba87cdcf48bb0635ecd8f
Your GitHub Usernamemsdubova <femme777@gmail.com>
2023-07-05 14:35:26 +0300

    Exercise 004

--- challenges/exercise004.js
+++ challenges/exercise004.js
@@ -1,41 +1,42 @@
 export function findSmallNums(nums) {
 	if (!nums) throw new Error('nums is required');
-	// Your code here
+	return nums.filter(item => item < 1);
 }
 
 export function findNamesBeginningWith(names, char) {
 	if (!names) throw new Error('names is required');
 	if (!char) throw new Error('char is required');
-	// Your code here
+	return names.filter(item => item.charAt(0) === char);
 }
 
 export function findVerbs(words) {
 	if (!words) throw new Error('words is required');
-	// Your code here
+	return words.filter(item => item.slice(0,3)==='to ')
 }
 
 export function getIntegers(nums) {
 	if (!nums) throw new Error('nums is required');
-	// Your code here
+	return nums.filter(item => Number.isInteger(item) )
 }
 
 export function getCities(users) {
 	if (!users) throw new Error('users is required');
-	// Your code here
+	return users.map(user => user.data.city.displayName);
 }
 
 export function getSquareRoots(nums) {
 	if (!nums) throw new Error('nums is required');
-	// Your code here
+	return nums.map(item => parseFloat((Math.sqrt(item)).toFixed(2)));
 }
 
 export function findSentencesContaining(sentences, str) {
 	if (!sentences) throw new Error('sentences is required');
 	if (!str) throw new Error('str is required');
-	// Your code here
+	return sentences.filter(item => item.toLowerCase().includes(str.toLowerCase()));
 }
 
 export function getLongestSides(triangles) {
 	if (!triangles) throw new Error('triangles is required');
-	// Your code here
+	return triangles.map(item => item.sort((a, b) => b - a)[0]);
 }
+	
