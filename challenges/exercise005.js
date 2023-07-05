3532032a12146f9ddf50dbe2c4e01f08ccf9d2c1
Your GitHub Usernamemsdubova <femme777@gmail.com>
2023-07-05 18:20:42 +0300

    Exercise 005: 1,3,4,5,6 tasks

--- challenges/exercise005.js
+++ challenges/exercise005.js
@@ -1,7 +1,7 @@
 export const findNextNumber = (nums, n) => {
 	if (nums === undefined) throw new Error('nums is required');
 	if (n === undefined) throw new Error('n is required');
-	// Your code here!
+	return nums.indexOf(n) > 0 ? nums.indexOf(n) !== (nums.length - 1) ? nums[nums.indexOf(n) + 1] : null : null;
 };
 
 export const count1sand0s = (str) => {
@@ -11,23 +11,41 @@ export const count1sand0s = (str) => {
 
 export const reverseNumber = (n) => {
 	if (n === undefined) throw new Error('n is required');
-	// Your code here!
+	return parseInt(String(Array.from(String(n)).map(item => parseInt(String(item))).reverse().join('')));
 };
 
 export const sumArrays = (arrs) => {
 	if (arrs === undefined) throw new Error('arrs is required');
-	// Your code here!
+	return (arrs.map(item => item.reduce(((a, b) => a + b), 0))).reduce(((c, d) => c + d), 0);
 };
 
 export const arrShift = (arr) => {
 	if (arr === undefined) throw new Error('arr is required');
-	// Your code here!
+	if (arr.length < 2) { return arr } else {
+		const swap = arr[0];
+		const swapLast = arr[arr.length - 1];
+		const shiftedArr = arr.slice(1, arr.length - 1);
+		shiftedArr.push(swap);
+		shiftedArr.unshift(swapLast);
+		return shiftedArr;
+	}
+
 };
 
 export const findNeedle = (haystack, searchTerm) => {
 	if (haystack === undefined) throw new Error('haystack is required');
 	if (searchTerm === undefined) throw new Error('searchTerm is required');
-	// Your code here!
+	
+	const arr = Object.values(haystack);
+
+  function isString(str) {
+    return typeof str === 'string' ? str.toLowerCase() : str;
+  }
+
+  const specificData = isString(searchTerm);
+  const keys = arr.map(item => isString(item));
+
+  return keys.some(item => typeof item === 'string' && item.includes(specificData));
 };
 
 export const getWordFrequencies = (str) => {
